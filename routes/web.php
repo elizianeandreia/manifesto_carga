ad1c665d8b503a1b32bf938eaad2e9658c8372cf
elizianeandreia <liziandreia@gmail.com>
2026-07-21 14:05:35 -0300

    edição

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,13 @@ Route::get('/', [ManifestoController::class, 'index'])
 
 Route::post('/manifestos', [ManifestoController::class, 'store'])
     ->name('manifestos.store');
+
+Route::get(
+    '/manifestos/{manifesto}/editar',
+    [ManifestoController::class, 'edit']
+)->name('manifestos.edit');
+
+Route::put(
+    '/manifestos/{manifesto}',
+    [ManifestoController::class, 'update']
+)->name('manifestos.update');
